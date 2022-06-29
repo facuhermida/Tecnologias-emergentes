799a893eaeefb2e7a9024caaf3ba7b36a584280c
facuhermida <facundo.hermida@usal.edu.ar>
2022-06-29 19:41:42 -0300

    Subscriber

--- routes/datos.py
+++ routes/datos.py
@@ -12,23 +12,6 @@ from sqlalchemy.sql import text
 
 appRouter = APIRouter()
 
-#SUBSCRIBE
-
-def on_message(client, userdata, message):
-    print("received message: " ,str(message.payload.decode("utf-8")))
-
-mqttBroker ="mqtt.eclipseprojects.io"
-
-client = mqtt.Client("Smartphone")
-client.connect(mqttBroker) 
-
-client.loop_start()
-
-client.subscribe("TEMPERATURE")
-client.on_message=on_message 
-
-time.sleep(30)
-client.loop_stop()
 
 @appRouter.get("/datos")
 async def get_datos():
