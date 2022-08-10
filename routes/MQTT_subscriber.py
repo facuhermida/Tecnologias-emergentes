be7da41522c7b5cef89010a939fcfef6e7b84c6b
facuhermida <facundo.hermida@usal.edu.ar>
2022-08-09 23:18:08 -0300

    asasas

--- routes/MQTT_subscriber.py
+++ routes/MQTT_subscriber.py
@@ -7,39 +7,39 @@ from config.database import insert_datos
 
 
 # MQTT Settings 
-MQTT_Broker = "mqtt.eclipseprojects.io"
-MQTT_Port = 1883
-Keep_Alive_Interval = 45
-MQTT_Topic = "Grupo2"
+# MQTT_Broker = "mqtt.eclipseprojects.io"
+# MQTT_Port = 1883
+# Keep_Alive_Interval = 45
+# MQTT_Topic = "Grupo2"
 
-#Subscribe to all Sensors at Base Topic
-def on_connect(mosq, obj, rc, properties=None):
-	mqttc.subscribe(MQTT_Topic, 0)
+# #Subscribe to all Sensors at Base Topic
+# def on_connect(mosq, obj, rc, properties=None):
+# 	mqttc.subscribe(MQTT_Topic, 0)
 
-#Save Data into DB Table
-def on_message(mosq, obj, msg):
-	# This is the Master Call for saving MQTT Data into DB
-	# For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
-	print ("MQTT Data Received...")
-	print ("MQTT Topic: " + msg.topic)  
-	print ("Data: " + msg.payload)
-	insert_datos(msg.payload)
+# #Save Data into DB Table
+# def on_message(mosq, obj, msg):
+# 	# This is the Master Call for saving MQTT Data into DB
+# 	# For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
+# 	print ("MQTT Data Received...")
+# 	print ("MQTT Topic: " + msg.topic)  
+# 	print ("Data: " + msg.payload)
+# 	insert_datos(msg.payload)
 
-def on_subscribe(mosq, obj, mid, granted_qos):
-    pass
+# def on_subscribe(mosq, obj, mid, granted_qos):
+#     pass
 
-mqttc = mqtt.Client()
+# mqttc = mqtt.Client()
 
-# Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_subscribe = on_subscribe
+# # Assign event callbacks
+# mqttc.on_message = on_message
+# mqttc.on_connect = on_connect
+# mqttc.on_subscribe = on_subscribe
 
-# Connect
-mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
+# # Connect
+# mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
 
-# Continue the network loop
-mqttc.loop_forever()
+# # Continue the network loop
+# mqttc.loop_forever()
 
 
 # def on_message(client, userdata, message):
